c7f19f6983fcf9096d86e6b6bee0666443cda609
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2020-09-29 17:20:14 +0300

    Updates

---
+++ precache-manifest.4a58bdbeed4ad296b52eeebb6999d9b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "863010c5b95e1c2dcfaca3156dcb01fb",
+    "url": "/goit-react-hw-08-phonebook/index.html"
+  },
+  {
+    "revision": "06d3fb57646f34b5b558",
+    "url": "/goit-react-hw-08-phonebook/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "f5dccabd76b374e10965",
+    "url": "/goit-react-hw-08-phonebook/static/css/main.53596b68.chunk.css"
+  },
+  {
+    "revision": "06d3fb57646f34b5b558",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.47085e52.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/goit-react-hw-08-phonebook/static/js/2.47085e52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5dccabd76b374e10965",
+    "url": "/goit-react-hw-08-phonebook/static/js/main.d2785dbe.chunk.js"
+  },
+  {
+    "revision": "2a13091ca041de263bee",
+    "url": "/goit-react-hw-08-phonebook/static/js/runtime-main.650e6e2b.js"
+  }
+]);
